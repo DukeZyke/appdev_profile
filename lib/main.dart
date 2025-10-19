bed6d9f26241983734bcee41844b6a319c0155d4
DukeZyke <zurielcalix414141@gmail.com>
2025-10-20 01:05:33 +0800

    finalized the layout and made information scrollable horizontally with each info having their own boxes

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp(home: Home(), debugShowCheckedModeBanner: false);
   }
 }
 
@@ -23,47 +20,130 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
-      ),
+        title: Text('Zuriel Eliazar D. Calix'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            CircleAvatar(
-              radius: 70,
-              backgroundImage: AssetImage('assets/profile.jpg'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Jane Doe',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Flutter Developer',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+            Container(
+              padding: EdgeInsets.all(50),
+              width: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: AssetImage('assets/profile.jpg'),
+                  ),
+                  Text(
+                    'Jane Doe',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
 
             SizedBox(height: 20),
-            Container(
-              color: Color(),
-              width: 400,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.email),
-                    title: Text('janedoe@gmail.com'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.phone),
-                    title: Text('09876543210'),
-                  ),  
-                  ListTile(
-                    leading: Icon(Icons.location_city),
-                    title: Text('Iloilo City, Philippines'),
-                  ),
-                ],
-              )
+            SizedBox(
+              width: 450,
+              height: 300,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(30),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(Icons.email),
+                            SizedBox(height: 8),
+                            Text('janedoe@gmail.com'),
+                          ],
+                        ),
+                      )
+                    ),
+                    SizedBox(width: 20),
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(Icons.phone),
+                            SizedBox(height: 8),
+                            Text('09876543210',),
+                          ],
+                        )  
+                      )
+                    ),
+                    SizedBox(width: 20),
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 2),
+                            ),
+                          ], 
+                        ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.location_city),
+                          SizedBox(height: 8),
+                          Text('Iloilo City, Philippines',),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ]
+                ),
+              ),
             ),
           ],
         ),
